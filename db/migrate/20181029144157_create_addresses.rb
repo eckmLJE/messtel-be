9aa39fe59c4a24a5b0c3a47c3f832f97acb6eb82
eckmLJE <lucas.eckman@gmail.com>
2018-10-30 12:47:18 -0400

    migrate

--- db/migrate/20181029144157_create_addresses.rb
+++ db/migrate/20181029144157_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
       t.string :name
-
+      t.string :mbid
       t.timestamps
     end
   end
